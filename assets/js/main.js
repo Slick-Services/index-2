300347bbc9ebd67886e4bc41b346b07e040657e7
Chetan Barot <chetan@slickservices.in>
2021-11-24 19:30:07 +0530

    model add

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,10 +254,10 @@
     });
 
     // jQuery counterUp
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 1000
-    });
+    // $('[data-toggle="counter-up"]').counterUp({
+    //     delay: 10,
+    //     time: 1000
+    // });
 
 
     // Porfolio isotope and filter
