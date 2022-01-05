a97109f1a420b2c5792f99a1ad13b9ed2e6ca6f0
Chetan Barot <chetan@slickservices.in>
2022-01-05 12:55:15 +0530

    style changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,8 +139,8 @@
      */
     on('click', '.mobile-nav-toggle', function(e) {
         select('#navbar').classList.toggle('navbar-mobile')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
+        this.classList.toggle('la-bars')
+        this.classList.toggle('la-times')
     })
 
     /**
