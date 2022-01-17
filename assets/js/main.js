4cf62de8bccb023597b320e60e65800a6b8bd461
Chetan Barot <chetan@slickservices.in>
2022-01-17 19:37:20 +0530

    all changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -378,5 +378,18 @@
             $('body').removeClass('expanded');
         });
     }
+    $(function() {
+
+        $('select[multiple].active.3col').multiselect({
+            columns: 2,
+            placeholder: 'Select States',
+            search: true,
+            searchOptions: {
+                'default': 'Search States'
+            },
+            selectAll: true
+        });
+
+    });
 
 })()
